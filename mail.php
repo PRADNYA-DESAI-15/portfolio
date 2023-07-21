31312a504263e5c19297b1460606dbcc6a328d70
Pradnya Desai <115803674+PRADNYA-DESAI-15@users.noreply.github.com>
2023-07-21 11:18:00 +0530

    php

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = $_POST['name'];
+$gmail = $_POST['gmail'];
+$contact = $_POST['contact'];
+$message = $_POST['message'];
+
+$to = "pradnyadesai72578@gmail.com";
+
+$subject = "mail from Portfolio";
+$txt = "Name = " , $name ,  "\r\n Email = ", $gmail , "\r\n Contact = ", $contact , "\r\n Message = ", $message ;
+
+$headers = "From: https://pradnya-desai-15.github.io/portfolio/ " ," \r\n" ,
+
+if($gmail!=NULL){
+    mail($to, $subject , $txt ,$headers);
+
+}
