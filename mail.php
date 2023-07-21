8ba5ec452bbc27b99592770774203b3684fd31c9
Pradnya Desai <115803674+PRADNYA-DESAI-15@users.noreply.github.com>
2023-07-21 11:23:25 +0530

    php

--- mail.php
+++ mail.php
@@ -5,12 +5,12 @@ $gmail = $_POST['gmail'];
 $contact = $_POST['contact'];
 $message = $_POST['message'];
 
-$to = "pradnyadesai7257@gmail.com";
+$to = "pradnyadesai72578@gmail.com";
 
 $subject = "mail from Portfolio";
 $txt = "Name = " , $name ,  "\r\n Email = ", $gmail , "\r\n Contact = ", $contact , "\r\n Message = ", $message ;
 
-$headers = "From: https://pradnya-desai-15.github.io/portfolio/ " ," \r\n" ,
+$headers = "From: noreply@https://pradnya-desai-15.github.io/portfolio/ " ," \r\n" ,
 
 if($gmail!=NULL){
     mail($to, $subject , $txt ,$headers);
